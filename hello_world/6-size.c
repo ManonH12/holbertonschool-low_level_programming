cfa73efe19b8d375ab0c2217d9525c596b9d7ab4
ManonH12 <9978@holbertonstudents.com>
2024-10-21 14:15:02 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - main function
  *
  * Return : o on success
- */ 
+ */
 int main(void)
 {
 	printf("Size of a char: $lu byte(s)", sizeof(char));
@@ -13,4 +13,4 @@ int main(void)
 	printf("Size of a long long int: $lu byte(s)", sizeof(long long int));
 	printf("Size of a float: $lu byte(s)", sizeof(float));
 	return (0);
-}	
+}
